ab247b3e4a0ce62f3cd2bf60d01e8d344ce33936
wwb <1252093436@qq.com>
2021-10-28 23:11:29 +0800

    打开新窗口测试

--- src/pages/detail/index.tsx
+++ src/pages/detail/index.tsx
@@ -1,5 +1,5 @@
 import { Component } from 'react'
-import { View, Text } from '@tarojs/components'
+import { View, Text, Button } from '@tarojs/components'
 import './index.styl'
 
 export default class Index extends Component {
@@ -14,11 +14,20 @@ export default class Index extends Component {
 
   componentDidHide () { }
 
+  handleClick = () => {
+    window.open("https://www.baidu.com", "_blank")
+  }
+
   render () {
     return (
       <View className='index'>
         <Text className="detail-title">详情页面</Text>
         <a href="https://www.baidu.com">https://www.baidu.com</a>
+        <Button
+          onClick={this.handleClick}
+          className="btn"
+          type="primary"
+        >在新窗口打开 baidu.com</Button>
       </View>
     )
   }
